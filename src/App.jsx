2126ac9221b835f299e02fa0bce5d7cb7bacbd0d
YashBodhe2004 <yashbodhe2004@gmail.com>
2024-01-29 18:02:54 +0530

    added component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import form from './components/form'
+
+function App() {
+  
+  return (
+    <>
+      <form />
+    </>
+  )
+}
+
+export default App
